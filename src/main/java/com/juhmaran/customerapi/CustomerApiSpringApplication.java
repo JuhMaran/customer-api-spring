6386cf4301a12708bb37825535882dfe1f1af39e
JuhMaran <julianemaran@gmail.com>
2026-04-02 09:50:12 -0300

    adding new project

---
+++ src/main/java/com/juhmaran/customerapi/CustomerApiSpringApplication.java
@@ -0,0 +1,13 @@
+package com.juhmaran.customerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerApiSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerApiSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/juhmaran/customerapi/CustomerApiSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juhmaran.customerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerApiSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
