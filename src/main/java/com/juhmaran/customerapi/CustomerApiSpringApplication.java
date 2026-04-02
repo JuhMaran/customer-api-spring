d2670792b5d32bd8dd1a8df51ce65a2712ec4c65
JuhMaran <julianemaran@gmail.com>
2026-04-02 20:13:30 -0300

    feat: implement caching for customer operations and configure logging with Logstash

--- src/main/java/com/juhmaran/customerapi/CustomerApiSpringApplication.java
+++ src/main/java/com/juhmaran/customerapi/CustomerApiSpringApplication.java
@@ -2,12 +2,14 @@ package com.juhmaran.customerapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
+@EnableCaching
 @SpringBootApplication
 public class CustomerApiSpringApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CustomerApiSpringApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(CustomerApiSpringApplication.class, args);
+  }
 
 }
